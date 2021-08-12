af57f7f76f1b51e24e788088b6d99f2f3d11f49b
gazdagergo <info@gazdagergo.com>
2021-08-12 22:42:01 +0200

    style(auth): indent to spaces

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -8,28 +8,28 @@ dotenv.config()
 const Usage = model('Usage', UsageSchema);
 
 module.exports = (req, res, next) => {
- 	if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-		req.user = { name: 'admin', roles: [ process.env.LOCALHOST_ROLE || 'admin' ] };
-		return next();
-	}
+   if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    req.user = { name: 'admin', roles: [ process.env.LOCALHOST_ROLE || 'admin' ] };
+    return next();
+  }
 
-	const token = req.headers.authorization;	
+  const token = req.headers.authorization;	
 
-	if (token) {
-		jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-			if (err) {
-				res.status(403);
-				return res.json({ error: 'Authorization token is invalid.' });
-			}
-			req.user = user;
-			const ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
-			next();
-			if (process.env.LOG_METRICS === 'true') {
-				Usage.insertMany([{ name: user.name, ip }])
-			}
-		});
-	} else {
-		res.status(401);
-		return res.json({ error: 'Authorization token is missing.' });
-	}
+  if (token) {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      if (err) {
+        res.status(403);
+        return res.json({ error: 'Authorization token is invalid.' });
+      }
+      req.user = user;
+      const ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
+      next();
+      if (process.env.LOG_METRICS === 'true') {
+        Usage.insertMany([{ name: user.name, ip }])
+      }
+    });
+  } else {
+    res.status(401);
+    return res.json({ error: 'Authorization token is missing.' });
+  }
 };
