c663da6687c4c95d339ce4a04dd9a4d1abc87ca3
Gergely Gazda <info@gazdagergo.com>
2022-04-11 08:59:03 +0200

    fix jsdoc

--- routes/szavazasEgyeb.js
+++ routes/szavazasEgyeb.js
@@ -6,6 +6,60 @@ const getPrevNextLinks = require('../functions/getPrevNextLinks')
 const parseStringObject = require('../functions/parseStringObject')
 
 
+/**
+* @api {get} /szavazas-egyeb/ 1.) Szavazás egyéb
+* @apiName szavazas-egyeb
+* @apiGroup 5. Szavazás egyéb
+* @apiDescription Az érvénytelen és lebélyegzetlen szavazólapok
+*
+* @apiParam (Request Parameters) {Number} [limit] Csak a megadott számú találatot adja vissza (default: `20`)
+* @apiParam (Request Parameters) {Number} [skip] A lapozáshoz használható paraméter. (default: `0`)
+* @apiParam (Request Parameters) {Number|String|Regex|Query} [queryParameters] A rekordok bármely paramétere alapján lehet szűkíteni a listát.
+* @apiHeader (Request Headers) Authorization A regisztrációkor kapott kulcs
+* @apiHeader (Request Headers) X-Valasztas-Kodja A választási adatbázis kiválasztása (Lehetsésges értékek: 2018-es országgyűlési: `ogy2018`, 2022-es országgyűlési: `ogy2022`)
+* @apiHeader (Response Headers) X-Total-Count A szűrési feltételeknek megfelelő, a válaszban lévő összes elem a lapozási beállításoktől függetlenül
+* @apiHeader (Request Headers) [X-Num-Parse] A numerikus értékeket integerként kezeli 
+* @apiHeader (Response Headers) X-Prev-Page A `limit` és `skip` paraméterekkel meghatározott lapozás következő oldala
+* @apiHeader (Response Headers) X-Next-Page A `limit` és `skip` paraméterekkel meghatározott lapozás előző oldala
+*
+* @apiSuccessExample {json} Success-Response:
+*  HTTP/1.1 200 OK
+*[
+*   {
+*       "_id": "6253a193e2cb28add339cd32",
+*       "szavazokor": {
+*           "_id": "620ed7332fdd2c590712bca7",
+*           "szavazokorSzama": "001",
+*           "kozigEgyseg": {
+*               "kozigEgysegNeve": "Budapest I. kerület",
+*               "telepulesKod": "001",
+*               "megyeKod": "01",
+*               "megyeNeve": "BUDAPEST",
+*               "letszam": {
+*                   "indulo": 20090,
+*                   "honos": 19809,
+*                   "atjel": 137,
+*                   "atjelInnen": 261,
+*                   "kuvi": 184,
+*                   "osszesen": 19946
+*               }
+*           },
+*           "valasztokerulet": {
+*               "szam": "01",
+*               "leiras": "BUDAPEST, 01. számú OEVK",
+*               "_id": "620ed5672fdd2c590712afd4"
+*           }
+*       },
+*       "darabszam": 12,
+*       "kategoria": "Érvénytelen szavazólapok",
+*       "jeloles": {
+*           "pozicio": "Egyéni választókerületi képviselő"
+*       },
+*       "__v": 0
+*   },
+* @apiSampleRequest off
+*/
+
 const router = express.Router();
 
 const DEFAULT_LIMIT = 20;
